ba527d17a907687b37ff02d970ad3fc0304a2c62
f4hm1N4d1 <fahminadi.314@gmail.com>
2025-05-09 17:11:49 +0700

    praktikum6

---
+++ vue-project/src/views/Education.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="container my-5" data-aos="fade-up">
+    <h2 class="h3 mb-4 text-primary">Pendidikan</h2>
+    <div class="row g-4">
+      <!-- Card 1 -->
+      <div class="col-md-6">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 class="card-title">Universitas Amikom yogyakarta</h5>
+            <p class="card-text">
+              <strong>Gelar:</strong> S.T. [Informatika]<br>
+              <strong>Tahun:</strong> 2023 – 2027
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Card 2 -->
+      <div class="col-md-6">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 class="card-title">SMA ABC</h5>
+            <p class="card-text">
+              <strong>Program:</strong> IPA<br>
+              <strong>Tahun:</strong> 2012 – 2015
+            </p>
+          </div>
+        </div>
+      </div>
+      <!--card 3-->
+      <div class="col-md-6">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 class="card-title">Universitas Amikom yogyakarta</h5>
+            <p class="card-text">
+              <strong>Gelar:</strong> S.T. [Informatika]<br>
+              <strong>Tahun:</strong> 2023 – 2027
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
---
+++ vue-project/src/views/Portfolio.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1 style = "text-align:center ">PORTOFOLIO</h1>
+    <p style = "text-align:center">berikut beberapa proyek yang telah dikembangkan </p>
+       <div class="row g-4">
+      <!-- Card 1 -->
+      <div class="col-md-6">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 style = "text-align:center"class="card-title">project tokopedia</h5>
+            <p class="card-text">
+
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Card 2 -->
+      <div class="col-md-6">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 style = "text-align:center"class="card-title">project tokopedia</h5>
+            <p class="card-text">
+
+            </p>
+          </div>
+        </div>
+      </div>
+      <!--card 3-->
+      <div class="col-md-6">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 style = "text-align:center"class="card-title">project tokopedia</h5>
+            <p class="card-text">
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
